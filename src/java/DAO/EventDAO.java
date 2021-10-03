986facb2b40c4a30f7b8ff6a16645ec9be388897
WilliamTrung <52268132+WilliamTrung@users.noreply.github.com>
2021-10-03 14:25:05 +0700

    Resolve Conflict EventDTO, EventDAO

--- src/java/DAO/EventDAO.java
+++ src/java/DAO/EventDAO.java
@@ -7,6 +7,7 @@ package DAO;
 
 import DTO.EventDTO;
 import DTO.LocationDTO;
+import DTO.SlotDTO;
 import DTO.UserDTO;
 import Utils.DBConnection;
 import static java.rmi.server.LogStream.log;
@@ -17,15 +18,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Admin
  */
 public class EventDAO {
-
+public int getLastId(){
+        //get the newest created id from tblEvents
+        //SELECT MAX(eventId) FROM tblEvents
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        int eventId = 0;
+        try {
+            conn = DBConnection.getConnection();
+            String sql = "SELECT MAX(eventId) FROM tblEvents";
+            stm = conn.prepareStatement(sql);
+            rs = stm.executeQuery();
+            if (rs.next()) {
+                eventId = rs.getInt("eventId");               
+            }
+        } catch (Exception e) {
+            log("Error at EventDAO - getLastId: " + e.toString());
+        } finally {
+            DBConnection.closeQueryConnection(conn, stm, rs);
+        }
+        return eventId;
+    }
     public List<EventDTO> getListEvent(String search) throws SQLException {
         List<EventDTO> list = new ArrayList<>();
         Connection conn = null;
@@ -33,26 +53,27 @@ public class EventDAO {
         ResultSet rs = null;
         try {
             conn = DBConnection.getConnection();
-            String sql = "SELECT eventId, userId, title, description, locationId, createDatetime, startDatetime, endDatetime, statusId, picture "
-                    + "FROM tblEvents WHERE title like ?";
+            String sql = "SELECT eventId, userId, title, description, locationId, createDatetime, slotId, s.statusName "
+                    + "FROM tblEvents e, tblStatusEvent s "
+                    + "WHERE title like ? AND s.statusId = e.statusId";
             stm = conn.prepareStatement(sql);
             stm.setString(1, "%" + search + "%");
             rs = stm.executeQuery();
             while (rs.next()) {
+                SlotDAO sDao = new SlotDAO();
                 int eventId = rs.getInt("eventId");
                 String userId = rs.getString("userId");
                 String title = rs.getString("title");
-                String description = rs.getString("description");
+                String description = rs.getString("description"); 
                 String locationId = rs.getString("locationId");
                 Date createDatetime = rs.getDate("createDatetime");
-                Date startDatetime = rs.getDate("startDatetime");
-                Date endDatetime = rs.getDate("endDatetime");
-                String picture = rs.getString("picture");
-                String statusId = rs.getString("statusId");
-
+                String slotId = rs.getString("slotId");
+                String status = rs.getString("statusName");
+                
                 UserDTO user = new UserDAO().getUserById(userId);
+                SlotDTO slot = sDao.getSlotById(slotId);
                 LocationDTO location = new LocationDAO().getLocationById(locationId);
-                list.add(new EventDTO(eventId, user, title, description, location, createDatetime, startDatetime, endDatetime, statusId, picture));
+                list.add(new EventDTO(eventId, user, title, description, location, createDatetime, slot, status));
             }
         } catch (Exception e) {
             log("Error at EventDAO - getListEvent: " + e.toString());
@@ -70,10 +91,10 @@ public class EventDAO {
         try {
             conn = DBConnection.getConnection();
             String sql = "WITH tblEventPage AS (SELECT (ROW_NUMBER() over (order by startDatetime) ) AS RowNum,\n"
-                    + "					eventId, userId, title, description, locationId, createDatetime, startDatetime, endDatetime, statusId, picture \n"
-                    + "				FROM tblEvents \n"
-                    + "				WHERE title like ?)\n"
-                    + "SELECT eventId, userId, title, description, locationId, createDatetime, startDatetime, endDatetime, statusId, picture\n"
+                    + "					eventId, userId, title, description, locationId, createDatetime, slotId, s.statusName AS status \n"
+                    + "				FROM tblEvents e, tblStatusEvent s \n"
+                    + "				WHERE title like ? AND e.statusId = s.statusId)\n"
+                    + "SELECT eventId, userId, title, description, locationId, createDatetime, slotId, status \n"
                     + "FROM tblEventPage WHERE RowNum BETWEEN ?*?-(?-1) AND ?*?";
             stm = conn.prepareStatement(sql);
             stm.setString(1, "%" + search + "%");
@@ -91,17 +112,16 @@ public class EventDAO {
                 String description = rs.getString("description");
                 String locationId = rs.getString("locationId");
                 Date createDatetime = rs.getDate("createDatetime");
-                Date startDatetime = rs.getDate("startDatetime");
-                Date endDatetime = rs.getDate("endDatetime");
-                String picture = rs.getString("picture");
-                String statusId = rs.getString("statusId");
+                String slotId = rs.getString("SlotId");
+                String status = rs.getString("status");
 
                 UserDTO user = new UserDAO().getUserById(userId);
                 LocationDTO location = new LocationDAO().getLocationById(locationId);
-                list.add(new EventDTO(eventId, user, title, description, location, createDatetime, startDatetime, endDatetime, statusId, picture));
+                SlotDTO slot = new SlotDAO().getSlotById(slotId);
+                list.add(new EventDTO(eventId, user, title, description, location, createDatetime, slot, status));
             }
         } catch (Exception e) {
-            log("Error at EventDAO - getListEvent: " + e.toString());
+            log("Error at EventDAO - getListEventByPage: " + e.toString());
         } finally {
             DBConnection.closeQueryConnection(conn, stm, rs);
         }
@@ -114,26 +134,22 @@ public class EventDAO {
         boolean flag = false;
         try {
             conn = Utils.DBConnection.getConnection1();
-            String sql = "INSERT INTO tblEvents (userId, title, description, locationId, createDatetime, startDatetime, endDatetime, statusId, picture) "
-                    + "VALUES (?,?,?,?,?,?,?)";
+            String sql = "INSERT INTO tblEvents (userId, title, description, locationId, createDatetime, slotId, statusId) "
+                    + "VALUES (?,?,?,?,?,?, (SELECT statusId FROM tblStatusEvent WHERE statusName = ?))";
             stm = conn.prepareStatement(sql);
             String userId = newEvent.getUser().getUserId();
             String title = newEvent.getTitle();
             String description = newEvent.getDescription();
             String locationId = newEvent.getLocation().getLocationId();
             Date createDatetime = newEvent.getCreateDatetime();
-            Date startDatetime = newEvent.getStartDatetime();
-            Date endDatetime = newEvent.getEndDatetime();
-            String picture = newEvent.getPicture();
+            String slotId = newEvent.getSlot().getSlotId();
             stm.setString(2, userId);
             stm.setString(3, title);
             stm.setString(4, description);
             stm.setString(5, locationId);
             stm.setDate(6, createDatetime);
-            stm.setDate(7, startDatetime);
-            stm.setDate(8, endDatetime);
+            stm.setString(7, slotId);
             stm.setString(9, "Pending");
-            stm.setString(10, picture);
             flag = stm.executeUpdate(sql) > 0;
         } catch (Exception e) {
             log("Error at EventDAO - createEvent: " + e.toString());
@@ -149,7 +165,7 @@ public class EventDAO {
         boolean check = false;
         try {
             conn = Utils.DBConnection.getConnection1();
-            String sql = "UPDATE tblEvents SET title=?, description=?, locationId=?, createDatetime=?, startDatetime=?, endDatetime=?, statusId = (SELECT statusId FROM tblStatusEvent WHERE statusName = ?), picture=?"
+            String sql = "UPDATE tblEvents SET title=?, description=?, locationId=?, slotId=?, statusId = (SELECT statusId FROM tblStatusEvent WHERE statusName = ?) "
                     + "WHERE eventId=? AND userId=?";
             stm = conn.prepareStatement(sql);
             int eventId = newEvent.getEventId();
@@ -157,21 +173,16 @@ public class EventDAO {
             String title = newEvent.getTitle();
             String description = newEvent.getDescription();
             String locationId = newEvent.getLocation().getLocationId();
-            Date createDatetime = newEvent.getCreateDatetime();
-            Date startDatetime = newEvent.getStartDatetime();
-            Date endDatetime = newEvent.getEndDatetime();
-            String statusId = newEvent.getStatus();
-            String picture = newEvent.getPicture();
+            String slotId= newEvent.getSlot().getSlotId();
+            String status = newEvent.getStatus();
+            
             stm.setString(1, title);
             stm.setString(2, description);
             stm.setString(3, locationId);
-            stm.setDate(4, createDatetime);
-            stm.setDate(5, startDatetime);
-            stm.setDate(6, endDatetime);
-            stm.setString(7, statusId);
-            stm.setString(8, picture);
-            stm.setInt(9, eventId);
-            stm.setString(10, userId);
+            stm.setString(5, slotId);
+            stm.setString(6, status);
+            stm.setInt(7, eventId);
+            stm.setString(8, userId);
             check = stm.executeUpdate(sql) > 0;
         } catch (Exception e) {
             log("Error at EventDAO - updateEvent: " + e.toString());
--- src/java/DTO/EventDTO.java
+++ src/java/DTO/EventDTO.java
@@ -7,124 +7,99 @@ package DTO;
 
 import java.sql.Date;
 
-
-
 /**
  *
  * @author Admin
  */
 public class EventDTO {
+
     private int eventId;
     private UserDTO user;
     private String title;
     private String description;
     private LocationDTO location;
     private Date createDatetime;
-    private Date startDatetime;
-
-    @Override
-    public String toString() {
-        return "EventDTO{" + "eventId=" + eventId + ", user=" + user + ", title=" + title + ", description=" + description + ", location=" + location + ", createDatetime=" + createDatetime + ", startDatetime=" + startDatetime + ", endDatetime=" + endDatetime + ", status=" + status + ", picture=" + picture + '}';
-    }
-    private Date endDatetime;
+    private SlotDTO slot;
     private String status;
-    private String picture;
 
-    public EventDTO(int eventId, UserDTO user, String title, String description, LocationDTO location, Date createDatetime, Date startDatetime, Date endDatetime, String status, String picture) {
+    public EventDTO(int eventId, UserDTO user, String title, String description, LocationDTO location, Date createDatetime, SlotDTO slot, String status) {
         this.eventId = eventId;
         this.user = user;
         this.title = title;
         this.description = description;
         this.location = location;
         this.createDatetime = createDatetime;
-        this.startDatetime = startDatetime;
-        this.endDatetime = endDatetime;
+        this.slot = slot;
         this.status = status;
-        this.picture = picture;
     }
 
-    
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-    
-    
     public int getEventId() {
         return eventId;
-    }
-
-    public void setEventId(int eventId) {
-        this.eventId = eventId;
     }
 
     public UserDTO getUser() {
         return user;
     }
 
-    public void setUser(UserDTO user) {
-        this.user = user;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public LocationDTO getLocation() {
         return location;
-    }
-
-    public void setLocation(LocationDTO location) {
-        this.location = location;
     }
 
     public Date getCreateDatetime() {
         return createDatetime;
     }
 
-    public void setCreateDatetime(Date createDatetime) {
-        this.createDatetime = createDatetime;
-    }
-
-    public Date getStartDatetime() {
-        return startDatetime;
-    }
-
-    public void setStartDatetime(Date startDatetime) {
-        this.startDatetime = startDatetime;
-    }
-
-    public Date getEndDatetime() {
-        return endDatetime;
-    }
-
-    public void setEndDatetime(Date endDatetime) {
-        this.endDatetime = endDatetime;
+    public SlotDTO getSlot() {
+        return slot;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String statusId) {
-        this.status = statusId;
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
     }
- 
-    
+
+    public void setUser(UserDTO user) {
+        this.user = user;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLocation(LocationDTO location) {
+        this.location = location;
+    }
+
+    public void setCreateDatetime(Date createDatetime) {
+        this.createDatetime = createDatetime;
+    }
+
+    public void setSlot(SlotDTO slot) {
+        this.slot = slot;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "EventDTO{" + "eventId=" + eventId + ", user=" + user + ", title=" + title + ", description=" + description + ", location=" + location + ", createDatetime=" + createDatetime + ", slot=" + slot.getSlotId() + ", status=" + status+'}';
+    }
+
 }
